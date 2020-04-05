78721b8bf491d9c23c539a512bccea0c730d8fcc
Marcel Garus <marcel.garus@gmail.com>
2020-04-06 01:18:08 +0200

    implement a rough version of formatting (not working yet)

--- parser/src/format.rs
+++ parser/src/format.rs
@@ -1,80 +1,169 @@
 use crate::parse::{Element, Element::*};
 use crate::utils::Single;
 
-pub fn format(elements: &[Element]) -> String {
-    let mut context = FormatContext {
-        // limit: 80,
-        string: "".to_string(),
-        indention: 0,
+pub fn format(elements: &[Element], context: &Context) -> String {
+    let response = elements.format(&Request {
+        context,
+        indentation_level: 0,
         offset: 0,
-    };
-    elements.format(&mut context);
-    context.string
+    });
+    response.text
 }
 
-type Indention = usize;
-
-struct FormatContext {
-    // limit: usize, // Soft horizontal character limit.
-    string: String,
-    indention: Indention,
-    offset: usize, // Offset from the left.
+pub struct Context {
+    pub limit: usize,        // Soft horizontal character limit.
+    pub indentation: String, // String that's used repeatedly to indent.
 }
 
-impl FormatContext {
-    fn push(&mut self, string: &str) {
-        self.ensure_indented();
-        self.string.push_str(string);
+struct Request<'a> {
+    context: &'a Context,
+    indentation_level: usize,
+    offset: usize,
+}
+
+impl<'a> Request<'a> {
+    fn indentation_string(&self) -> String {
+        self.context.indentation.repeat(self.indentation_level)
     }
-    fn ensure_indented(&mut self) {
-        if self.offset == 0 {
-            self.push(&"  ".repeat(self.indention))
-        }
-    }
+}
+
+struct Response {
+    text: String,
+    inlining: Inlining,
+}
+
+// This is a trivial block with only one body that contains text. These
+// kind of blocks get inlined:
+// ```mlml
+// This is text with some bold{blocks that are
+// formatted inline}. Notice that italic{their
+// body text may break}.
+// ```
+enum Inlining {
+    NotInlinable(),
+    Inlined(usize), // Contains the new offset.
 }
 
 trait Formattable {
-    fn format(&self, context: &mut FormatContext);
-}
-
-impl Formattable for &[Element] {
-    fn format(&self, context: &mut FormatContext) {
-        for (i, element) in self.iter().enumerate() {
-            if i > 0 {
-                context.push(" ");
-            }
-            element.format(context);
-        }
-    }
+    fn format(&self, request: &Request) -> Response;
 }
 
 impl Formattable for Element {
-    fn format(&self, context: &mut FormatContext) {
+    fn format(&self, request: &Request) -> Response {
         match self {
-            Text(text) => format_text(text, context),
-            Block(name, content) => format_block(name, content, context),
+            Text(text) => {
+                let words: Vec<&str> = text.split_whitespace().collect();
+                wrap_parts(&words, request)
+            }
+            Block(name, bodies) => {
+                if let Some(Some(Text(text))) = bodies.single().map(|body| body.single()) {
+                    let text = format!("{}{{ {} }}", name, text);
+                    let parts: Vec<&str> = text.split_whitespace().collect();
+                    wrap_parts(&parts, request)
+                } else {
+                    let body_request = Request {
+                        indentation_level: request.indentation_level + 1,
+                        offset: 0,
+                        ..*request
+                    };
+                    let body_responses = bodies.iter().map(|body| body.format(&body_request));
+                    let mut text = request.indentation_string();
+                    text.push_str(name);
+                    let mut offset = text.len();
+                    for response in body_responses {
+                        let body_text = response.text;
+                        // Check if the body would fit into the same line. That
+                        // means, it is only one line and if we would surround
+                        // it with " { " and " }" (5 characters in total), it
+                        // would still be less than the limit and leave two
+                        // characters left for the next body's start.
+                        if !body_text.contains('\n')
+                            && offset + 5 + body_text.len() < request.context.limit - 2
+                        {
+                            text.push_str(" {{");
+                            text.push_str(&body_text);
+                            text.push_str(" }}");
+                            offset += 5 + body_text.len();
+                        } else {
+                            text.push_str(" {\n");
+                            text.push_str(&body_request.indentation_string());
+                            text.push_str(&body_text);
+                            text.push_str("\n");
+                            text.push_str(&request.indentation_string());
+                            text.push_str("}");
+                            offset = request.indentation_string().len() + 1;
+                        }
+                    }
+                    Response {
+                        text,
+                        inlining: Inlining::NotInlinable(),
+                    }
+                }
+            }
         }
     }
 }
 
-fn format_text(text: &str, context: &mut FormatContext) {
-    context.push(&text)
+impl Formattable for [Element] {
+    fn format(&self, request: &Request) -> Response {
+        // Inlining of single-text-only bodies needs to be handled at block-
+        // level, because the block also needs to be inlined. So here, we know
+        // for sure we're not being displayed inline.
+        assert_eq!(request.offset, 0);
+
+        let mut text = "".to_string();
+        let mut offset = 0;
+
+        for element in self {
+            let response = element.format(&Request { offset, ..*request });
+            match response.inlining {
+                Inlining::NotInlinable() => {
+                    text.push_str("\n");
+                    text.push_str(&request.indentation_string());
+                    text.push_str(&response.text);
+                }
+                Inlining::Inlined(new_offset) => {
+                    text.push_str(&response.text);
+                    offset = new_offset;
+                }
+            }
+        }
+
+        Response {
+            text,
+            inlining: Inlining::NotInlinable(),
+        }
+    }
 }
 
-fn format_block(name: &str, content: &[Element], context: &mut FormatContext) {
-    if content.is_empty() {
-        context.push(&format!("{}{{}}", name));
-    } else if let Some(single_element) = content.single() {
-        context.push(&format!("{}{{{}}}", name, single_element.format(context)));
-        format!(
-            "{}{}{{{}}}",
-            context.indent(),
-            name,
-            format_text(&text, context)
-        )
-    } else {
-        let indent = context.indent();
-        context.indention += 1;
-        format!("{}{} {{\n{}\n}}", indent, name, &content.format(context))
+fn wrap_parts(parts: &[&str], request: &Request) -> Response {
+    let indentation = request.indentation_string();
+    let mut lines: Vec<String> = vec![];
+    let mut line = "".to_string();
+    let mut offset = request.offset;
+    for part in parts {
+        if offset + 1 + part.len() <= request.context.limit {
+            // The word still fits in the line.
+            line.push(' ');
+            line.push_str(&part);
+            offset += 1 + part.len();
+        } else {
+            // The word doesn't fit in the line anymore.
+            if offset != 0 {
+                // If this is the only word in this line though, we still need
+                // to squeeze it in, because it won't fit in the next lines
+                // either (we remain at the same indentation).
+                lines.push(line);
+                line = indentation.clone();
+                offset = indentation.len();
+            }
+            line.push_str(&part);
+            offset += part.len();
+        }
+    }
+
+    Response {
+        text: lines.join("\n"),
+        inlining: Inlining::Inlined(offset),
     }
 }
