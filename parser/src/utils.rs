5c8d3b3658d965a4257bb6f70527c79428ed574c
Marcel Garus <marcel.garus@gmail.com>
2020-04-06 01:16:55 +0200

    implement single for the slice type itself, not for a borrowed version

--- parser/src/utils.rs
+++ parser/src/utils.rs
@@ -2,7 +2,7 @@ pub trait Single<T> {
     fn single(&self) -> Option<&T>;
 }
 
-impl<T> Single<T> for &[T] {
+impl<T> Single<T> for [T] {
     fn single(&self) -> Option<&T> {
         if self.len() == 1 {
             Some(&self[0])
