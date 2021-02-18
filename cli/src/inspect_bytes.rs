1ac3e5500bfade4d9d9b9645e583099d2611d2ff
Marcel Garus <marcel.garus@gmail.com>
2021-02-18 17:17:16 +0100

    Add syntax highlighting for FewBytes atoms

--- cli/src/inspect_bytes.rs
+++ cli/src/inspect_bytes.rs
@@ -1,12 +1,14 @@
 use colored::{Color, Colorize};
 use semdoc_engine::atoms::*;
 use semdoc_engine::utils::*;
+use std::cmp::min;
 use std::convert::TryInto;
 
 enum WordInfo {
     Header { version: u16 },
     Block { kind: u64, num_children: u8 },
     Bytes { length: u64 },
+    FewBytes { length: u8 },
     BytesContinuation { num_relevant: u8, is_last: bool },
 }
 
@@ -15,6 +17,24 @@ fn info_for_bytes(bytes: &[u8]) -> Vec<WordInfo> {
         version: u16::from_be_bytes(bytes[6..8].try_into().unwrap()),
     }];
     // TODO(marcelgarus): Handle too large version.
+
+    let add_byte_continuations = |length: usize, info: &mut Vec<WordInfo>| {
+        let num_payload_words = length.round_up_to_multiple_of(8) / 8;
+        for i in 0..num_payload_words {
+            info.push(if i == num_payload_words - 1 {
+                WordInfo::BytesContinuation {
+                    num_relevant: (length % 8) as u8,
+                    is_last: true,
+                }
+            } else {
+                WordInfo::BytesContinuation {
+                    num_relevant: 8,
+                    is_last: false,
+                }
+            });
+        }
+    };
+
     let mut cursor = 8;
     while cursor < bytes.len() {
         let atom = Atom::from_bytes(&bytes[cursor..]).unwrap();
@@ -26,20 +46,13 @@ fn info_for_bytes(bytes: &[u8]) -> Vec<WordInfo> {
                 info.push(WordInfo::Bytes {
                     length: bytes.len() as u64,
                 });
-                let num_payload_words = bytes.len().round_up_to_multiple_of(8) / 8;
-                for i in 0..num_payload_words {
-                    info.push(if i == num_payload_words - 1 {
-                        WordInfo::BytesContinuation {
-                            num_relevant: (bytes.len() % 8) as u8,
-                            is_last: true,
-                        }
-                    } else {
-                        WordInfo::BytesContinuation {
-                            num_relevant: 8,
-                            is_last: false,
-                        }
-                    });
-                }
+                add_byte_continuations(bytes.len(), &mut info);
+            }
+            Atom::FewBytes(bytes) => {
+                info.push(WordInfo::FewBytes {
+                    length: bytes.len() as u8,
+                });
+                add_byte_continuations(bytes.len() - 6, &mut info); // 6 bytes are already stored right here.
             }
         };
     }
@@ -69,6 +82,15 @@ impl WordInfo {
             WordInfo::Bytes { .. } => [
                 atom_kind, length, length, length, length, length, length, length,
             ],
+            WordInfo::FewBytes { length: len } => {
+                let mut colors = [padding; 8];
+                for i in 0..min(2 + *len as usize, 8) {
+                    colors[i] = payload;
+                }
+                colors[0] = atom_kind;
+                colors[1] = length;
+                colors
+            }
             WordInfo::BytesContinuation { num_relevant, .. } => {
                 let mut colors = [padding; 8];
                 for i in 0..(*num_relevant as usize) {
@@ -123,10 +145,17 @@ pub fn inspect_bytes(file: &str) {
 }
 impl WordInfo {
     fn starts_atom(&self) -> bool {
-        matches!(self, WordInfo::Block { .. } | WordInfo::Bytes { .. })
+        matches!(
+            self,
+            WordInfo::Block { .. } | WordInfo::Bytes { .. } | WordInfo::FewBytes { .. }
+        )
     }
     fn ends_atom(&self) -> bool {
-        matches!(self, WordInfo::BytesContinuation { is_last, .. } if *is_last)
+        match self {
+            WordInfo::BytesContinuation { is_last, .. } if *is_last => true,
+            WordInfo::FewBytes { length } if *length <= 6 => true,
+            _ => false,
+        }
     }
 }
 
@@ -198,6 +227,11 @@ fn stringify_info(info: &WordInfo) -> String {
         WordInfo::Bytes { length } => format!(
             "{}{}",
             "Bytes".color(colors::atom_kind).bold(),
+            format!(", {} long", length).color(colors::length),
+        ),
+        WordInfo::FewBytes { length } => format!(
+            "{}{}",
+            "FewBytes".color(colors::atom_kind).bold(),
             format!(", {} long", length).color(colors::length),
         ),
         WordInfo::BytesContinuation { num_relevant, .. } => format!(
