e880dcf44697f4ed2f6b6833fe0fd2b5eaf17957
Marcel Garus <marcel.garus@gmail.com>
2021-03-10 00:01:02 +0100

    Fix formatting

--- convert/markdown-to-semdoc/src/lib.rs
+++ convert/markdown-to-semdoc/src/lib.rs
@@ -1,13 +1,16 @@
+use std::cell::RefCell;
+
+use comrak::{
+    arena_tree::Children,
+    nodes::{Ast, AstNode, ListType, NodeValue},
+    parse_document, Arena, ComrakOptions,
+};
 use semdoc::{Block, Pure, SemDoc};
-use comrak::{ComrakOptions, Arena, parse_document,  nodes::{AstNode, NodeValue, ListType}};
 
 pub fn markdown_to_semdoc(markdown: &str) -> SemDoc<Pure> {
     let arena = Arena::new();
 
-    let root = parse_document(
-        &arena,
-        markdown,
-        &ComrakOptions::default(),);
+    let root = parse_document(&arena, markdown, &ComrakOptions::default());
 
     SemDoc::new(root.to_block())
 }
@@ -19,64 +22,49 @@ impl<'a> ToBlock<'a> for AstNode<'a> {
     fn to_block(&'a self) -> Block<Pure> {
         use NodeValue::*;
         match self.data.borrow().value.clone() {
-            Document => {
-                Block::Paragraphs(self.children().collect::<Vec<_>>().clone().to_blocks())
+            Document => Block::Paragraphs(self.children().to_blocks()),
+            Heading(_) => Block::Section {
+                title: Box::new(Block::Paragraphs(self.children().to_blocks())),
+                body: Box::new(Block::Empty),
             },
-            Heading(_) => {
-                Block::Section {
-                    title: Box::new(Block::Paragraphs(self.children().collect::<Vec<_>>().clone().to_blocks())),
-                    body: Box::new(Block::Empty),
-                }
-            },
-            Paragraph => {
-                Block::Flow(self.children().collect::<Vec<_>>().clone().to_blocks())
-            },
-            Text(text) => {
-                Block::Text(String::from_utf8(text).unwrap())
-            },
+            Paragraph => Block::Flow(self.children().to_blocks()),
+            Text(text) => Block::Text(String::from_utf8(text).unwrap()),
             SoftBreak => Block::Text(" ".to_owned()),
-            Emph => {
-                // TODO(marcelgarus): Handle emphasis.
-                Block::Flow(self.children().collect::<Vec<_>>().clone().to_blocks())
-            }
-            Strong => {
-                // TODO(marcelgarus): Handle strong text.
-                Block::Flow(self.children().collect::<Vec<_>>().clone().to_blocks())
-            }
+            // TODO(marcelgarus): Handle emphasis.
+            Emph => Block::Flow(self.children().to_blocks()),
+            // TODO(marcelgarus): Handle strong text.
+            Strong => Block::Flow(self.children().to_blocks()),
             List(list) => {
-                let items = self.children().collect::<Vec<_>>().clone().to_blocks();
+                let items = self.children().to_blocks();
                 println!("List type is {:?}", list.list_type);
                 match list.list_type {
                     ListType::Bullet => Block::BulletList(items),
                     ListType::Ordered => Block::OrderedList(items),
                 }
-            },
-            Item(_) => {
-                Block::Paragraphs(self.children().collect::<Vec<_>>().clone().to_blocks())
-            },
-            HtmlBlock(_) => {
-                // TODO(marcelgarus): Handle HTML better.
-                Block::Empty
-            },
+            }
+            Item(_) => Block::Paragraphs(self.children().to_blocks()),
+            // TODO(marcelgarus): Handle HTML better.
+            HtmlBlock(_) => Block::Empty,
             ThematicBreak => Block::Empty,
-            Link(link) => {
-                // TODO(marcelgarus): Handle links better.
-                Block::Text(String::from_utf8(link.title).unwrap())
-            },
+            // TODO(marcelgarus): Handle links better.
+            Link(link) => Block::Text(String::from_utf8(link.title).unwrap()),
             _ => {
                 println!("Not handling node {:?} yet.", self);
                 Block::Empty
             }
         }
     }
-
 }
 trait ToBlocks {
-    fn to_blocks(&self) -> Vec<Block<Pure>>;
+    fn to_blocks(self) -> Vec<Block<Pure>>;
 }
-impl<'a> ToBlocks for [&'a AstNode<'a>] {
-    fn to_blocks(&self) -> Vec<Block<Pure> >{
+impl<'a> ToBlocks for Vec<&'a AstNode<'a>> {
+    fn to_blocks(self) -> Vec<Block<Pure>> {
         self.iter().map(|node| node.to_block()).collect()
     }
-
+}
+impl<'a> ToBlocks for Children<'a, RefCell<Ast>> {
+    fn to_blocks(self) -> Vec<Block<Pure>> {
+        self.collect::<Vec<_>>().clone().to_blocks()
+    }
 }
